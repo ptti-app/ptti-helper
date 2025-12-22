eedc06cfead522478ccf10332fb56a8275b16924
Rafiq Arbee <rfqarbee@gmail.com>
2025-12-22 21:56:17 +0800

    update helper

--- log_json.go
+++ log_json.go
@@ -6,11 +6,15 @@ import (
 )
 
 // convert to JSON and log out
-func Log(T any) {
+func Log(T any, customMsg ...string) {
 	json, err := json.MarshalIndent(T, "", " ")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Results: %v\n", string(json))
+	if customMsg != nil {
+		log.Printf("%s: %v\n", customMsg, string(json))
+	} else {
+		log.Printf("Results: %v\n", string(json))
+	}
 }
